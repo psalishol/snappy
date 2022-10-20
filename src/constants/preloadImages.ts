ce00fb567927c627d69a5c685008d9555f490642
Psalishol samuel <psalishol80@gmail.com>
2022-10-20 23:04:35 +0100

    preloads images

---
+++ src/constants/preloadImages.ts
@@ -0,0 +1,13 @@
+export const china = require("../../assets/china.png");
+export const australia = require("../../assets/australia.png");
+export const canada = require("../../assets/canada.png");
+export const england = require("../../assets/england.png");
+export const germany = require("../../assets/germany.png");
+export const nigeria = require("../../assets/nigeria.png");
+export const southKorea = require("../../assets/southKorea.png");
+export const southAfrica = require("../../assets/southAfrica.png");
+export const america = require("../../assets/america.png");
+
+export const goPremium = require("../../assets/goPremium.png");
+export const dashboard = require("../../assets/dashboard.png");
+export const map = require("../../assets/world-map.png");
