c10f1f44d96e51b16059f71dda8c21a853b111b0
Psalishol samuel <psalishol80@gmail.com>
2022-10-21 06:07:17 +0100

    Added map translation

--- src/components/screenBackground/screenBackground.tsx
+++ src/components/screenBackground/screenBackground.tsx
@@ -1,3 +1,4 @@
+import { MotiView } from "moti";
 import { View, Platform, StatusBar, Image, StyleSheet } from "react-native";
 import { Colors } from "../../constants";
 import { map } from "../../constants/preloadImages";
@@ -7,7 +8,7 @@ interface ScreenBackgroundProps {
 }
 
 const ScreenBackground = ({ children }: ScreenBackgroundProps) => {
-  // const offsetAngle = Math.PI / N;
+  const animateBackground = true;
   return (
     <>
       <StatusBar
@@ -15,7 +16,25 @@ const ScreenBackground = ({ children }: ScreenBackgroundProps) => {
         backgroundColor={Colors.PRIMARY_BACKGROUND_COLOR}
       />
       <View style={styles.view}>
-        <Image style={styles.background} source={map} />
+        <MotiView
+          from={{ translateX: 0 }}
+          animate={{
+            translateX: [
+              -5, -10, -15, -20, -25, -30, -35, -40, -45, -50, -55, -60, -65,
+              -70, -75, -80, -85, -90, -95, -100, -105, -110, -115, -120, -125,
+              -130, -135, -140, -145, -150, -155, -160, -165, -170, -175, -180,
+              // -185, -190, -195, -200, -205, -210, -215, -220, -225, -230, -235,
+              // -240, -245, -250,
+            ],
+          }}
+          transition={{
+            loop: animateBackground,
+            repeatReverse: false,
+            damping: 300,
+          }}
+        >
+          <Image resizeMode="cover" style={styles.background} source={map} />
+        </MotiView>
         {children}
       </View>
     </>
@@ -34,8 +53,9 @@ const styles = StyleSheet.create({
     right: 0,
     left: 0,
     height: 300,
-    width: "100%",
+    width: 600,
     opacity: 0.1,
+    transform: [{ translateX: -13 }],
   },
 });
 
