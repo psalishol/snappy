9c86547983181a39ffc62592cb8349bcf4533390
Psalishol samuel <psalishol80@gmail.com>
2022-10-21 05:54:41 +0100

    Introduced the context, used it for changing the connection text

--- src/components/statusText/statusText.tsx
+++ src/components/statusText/statusText.tsx
@@ -1,18 +1,32 @@
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { Colors } from "../../constants";
+import { MotiView } from "moti";
+import AnimatedDot from "./AnimatedConnectingDot";
+import { ConnectionStatusContext } from "../../Global/ConnectionStatusContext";
 
 export default function StatusText() {
+  const { connectionStatus } = useContext(ConnectionStatusContext);
+  const displayText =
+    connectionStatus === "Disconnected"
+      ? "Not Connected"
+      : connectionStatus === "Connecting"
+      ? "Connecting"
+      : "Connected";
+  const color = connectionStatus === "Connecting" ? Colors.YELLO : "white";
+  const showLoading = connectionStatus === "Connecting" ? true : false;
   return (
-    <Text
-      style={{
-        color: Colors.YELLO,
-        fontSize: 30,
-        fontWeight: "bold",
-        textAlign: "center",
-        marginBottom: 20,
-      }}
-    >
-      Connecting...
-    </Text>
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <Text style={[styles.text, { color }]}>{displayText}</Text>
+      {showLoading && <AnimatedDot />}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
