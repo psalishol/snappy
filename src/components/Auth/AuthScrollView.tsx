8b64ed520f4bf0d761f8cd47f392c17812ef4ac8
Psalishol samuel <psalishol80@gmail.com>
2022-10-22 10:47:53 +0100

    Auth scroll view

---
+++ src/components/Auth/AuthScrollView.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, useState, useEffect, Children } from "react";
+import { View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+import { screenWidth } from "../../constants/screenDimensions";
+
+interface AuthScrollViewProps {
+  index: number;
+  children: ReactNode;
+}
+
+const AuthScrollView = ({ index, children }: AuthScrollViewProps) => {
+  const viewDimension = screenWidth * 2;
+  const translation = useSharedValue(0);
+
+  const translationX = useAnimatedStyle(() => {
+    return { transform: [{ translateX: translation.value }] };
+  });
+
+  useEffect(() => {
+    translation.value = withSpring(index === 0 ? 0 : -screenWidth, {
+      damping: 300,
+    });
+  }, [index]);
+  return (
+    <Animated.View
+      style={[
+        { width: viewDimension, height: "100%", flexDirection: "row" },
+        translationX,
+      ]}
+    >
+      {Children.map(children, (child) => {
+        return (
+          <View style={{ width: screenWidth, marginTop: 300 }}>{child}</View>
+        );
+      })}
+    </Animated.View>
+  );
+};
+
+export default AuthScrollView;
