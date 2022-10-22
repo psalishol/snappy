be44a15bb37d3c77779db81d65a9259ee0f8efbd
Psalishol samuel <psalishol80@gmail.com>
2022-10-22 07:20:25 +0100

    Index file for the animated logo components

---
+++ src/components/splashSplash/Logo/index.ts
@@ -0,0 +1,5 @@
+import AnimatedSpacer from "./AnimatedSpacer";
+import AnimatedLogoContainer from "./AnimatedLogoContainer";
+import LogoText from "./LogoText";
+
+export { AnimatedSpacer, AnimatedLogoContainer, LogoText };
